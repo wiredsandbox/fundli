f1d4717b05d8eff80a88b89ad8539fcd3e5743a2
owoborode Oluwaseye <owoborodeseye@gmail.com>
2023-03-13 17:20:27 +0100

    create login endpoint

--- pocketguardapp/account.py
+++ pocketguardapp/account.py
@@ -3,7 +3,7 @@ from .schemas.account_schemas import (
     AccountRequest,
     AccountResponse,
     AccountLoginRequest,
-    account_response_serializer,
+    account_auth_response_serializer,
 )
 from .services import account as account_service
 
@@ -29,5 +29,21 @@ async def signup(request: AccountRequest):
         account.email, account.first_name, account.last_name
     )
 
-    #  map account to account response
-    return account_response_serializer(account, token)
+    return account_auth_response_serializer(account, token)
+
+
+@account_router.post("/login", response_model=AccountResponse)
+async def login(request: AccountLoginRequest):
+    account, error = account_service.login_account(
+        email=request.email, password=request.password
+    )
+    if error:
+        raise HTTPException(status_code=error.code, detail=error.msg)
+
+    token = account_service.generate_token(
+        account.email, account.first_name, account.last_name
+    )
+
+    return account_auth_response_serializer(account, token)
+
+
--- pocketguardapp/schemas/account_schemas.py
+++ pocketguardapp/schemas/account_schemas.py
@@ -21,8 +21,17 @@ class AccountResponse(BaseModel):
     last_name: str
     token: str
 
+    class config:
+        orm_mode = True
+        example = {
+            "email": "example@example.com",
+            "id": "5f9f1c5b9c9d4b0b8c1c1c1c",
+            "first_name": "John",
+            "last_name": "Doe",
+        }
 
-def account_response_serializer(account: Account, token: str):
+
+def account_auth_response_serializer(account: Account, token: str = None):
     """account_response_serializer serializes an account to an AccountResponse"""
 
     return AccountResponse(token=token, id=str(account.id), **account.to_dict())
