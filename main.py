f8100f4615f035d0f76717d172dec8c8d1c92384
owoborode Oluwaseye <owoborodeseye@gmail.com>
2023-03-29 19:25:54 +0100

    Feature
    Create email service

--- main.py
+++ main.py
@@ -37,6 +37,27 @@ async def get_file():
     return FileResponse(TEMPLATE_FOLDER + "/email/images/logo.svg")
 
 
+<<<<<<< HEAD
+=======
+from fundli.email_service.email_schema import EmailSchema
+from fundli.email_service.email_service import send_email
+
+
+@app.get("/email")
+def email():
+    body = EmailSchema(
+        email_template="welcome.html",
+        subject="Welcome to fundli",
+        sender_name="Favour from Fundli",
+        sender_email="info@fundli.live",
+        first_name="Favour",
+        recipients=["owoborodeseye@gmail.com", "bunmioye09@gmail.com"],
+    )
+    s = send_email(body)
+    return s.json()
+
+
+>>>>>>> 5c19080 (Feature)
 if __name__ == "__main__":
     # start app
     uvicorn.run("main:app", port=PORT, reload=True)
